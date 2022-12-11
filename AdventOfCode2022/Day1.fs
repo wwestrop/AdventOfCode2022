428535b8418682ebf6596994c747fcc80fb53599
Will Westrop <wwestrop@hotmail.com>
2022-12-11 17:10:24 +0000

    Day 1, part 1

---
+++ AdventOfCode2022/Day1.fs
@@ -0,0 +1,39 @@
+﻿open System.IO
+
+let mutable input = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"
+
+
+input <- File.ReadAllText("input.txt");
+
+
+let day1 (input: string) =          // TODO parens enable type hint. Does it also create a tuple?
+    let lines = input.Split("\r\n")
+    
+    let getElves =
+        let mutable acc = 0         // hmmmmm, mutable...........
+        seq {
+            for l in lines do
+                match l with
+                | "" -> yield acc; acc <- 0         // this assumes the file ends with a blank line
+                | x -> acc <- acc + int x
+        }
+
+    let elvesCalories = getElves
+    Seq.max(elvesCalories)
+
+
+
+printfn "%i" (day1 input)
