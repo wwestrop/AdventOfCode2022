54b50d8c9a9a1222a1b7a09aecbb97fb7335f5dd
Will Westrop <wwestrop@hotmail.com>
2022-12-12 19:22:46 +0000

    Day 2, part 2

--- AdventOfCode2022/Day2.fs
+++ AdventOfCode2022/Day2.fs
@@ -24,6 +24,50 @@ let leftHandWins = [
     ("scissors", "paper")
 ]
 
+let allMoves = [ "rock"; "paper"; "scissors" ]
+
+
+let getWinningMove move =
+    let (l, _) = leftHandWins |> List.filter (fun (_, r) -> r = move) |> List.head
+    l
+
+
+
+let getLosingMove (move: string) =
+    let winningMove = getWinningMove move
+    allMoves |> List.except [move] |> List.except [winningMove] |> List.head
+
+
+
+let getCorrespondingMove theirMove desiredOutcome =
+    match desiredOutcome with
+    | 'Y' -> theirMove                      // draw
+    | 'Z' -> getWinningMove theirMove       // win
+    | 'X' -> getLosingMove theirMove        // lose
+ 
+
+
+let score (round: string * string) =
+    let shapeScore =
+        match round with
+        | _, "rock" -> 1
+        | _, "paper" -> 2
+        | _, "scissors" -> 3
+
+    let outcomeScore =
+        let (x, _) = round             // this is rindonkulously painful
+        let (_, y) = round
+
+        let isDraw = x = y            
+        let isLoss = List.contains round leftHandWins
+
+        match isDraw, isLoss with
+        | true, _ -> 3
+        | _, true -> 0
+        | _ -> 6
+
+    shapeScore + outcomeScore
+
 
 let day2 (input: string) =
 
@@ -37,32 +81,28 @@ let day2 (input: string) =
             for l in lines -> parse l
         }
 
-    let score (round: string * string) =
-        let shapeScore =
-            match round with
-            | _, "rock" -> 1
-            | _, "paper" -> 2
-            | _, "scissors" -> 3
+    roundsPlayed |> Seq.map score |> Seq.sum
 
-        let outcomeScore =
-            let (x, _) = round             // this is rindonkulously painful
-            let (_, y) = round
 
-            let isDraw = x = y            
-            let isLoss = List.contains round leftHandWins
 
-            match isDraw, isLoss with
-            | true, _ -> 3
-            | _, true -> 0
-            | _ -> 6
+let day2part2 (input: string) =
 
-        shapeScore + outcomeScore
+    let lines = input.Replace(" ", "").Split("\r\n")
+
+    let parse (line: string) =
+        let l = lookup.[line[0]]
+        let rEncrypted = line[1]
+        let r = getCorrespondingMove l rEncrypted
+        l, r
+
+    let roundsPlayed =
+        seq {
+            for l in lines -> parse l
+        }
 
     roundsPlayed |> Seq.map score |> Seq.sum
 
 
 
 printfn "%i" (day2 input)
-
-
-// 13809
+printfn "%i" (day2part2 input)
