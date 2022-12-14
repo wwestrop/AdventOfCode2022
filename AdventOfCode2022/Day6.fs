da265ccf892638db5ee78c37beba88556c3f8cea
Will Westrop <wwestrop@hotmail.com>
2022-12-14 22:45:28 +0000

    Day 6, part 2

--- AdventOfCode2022/Day6.fs
+++ AdventOfCode2022/Day6.fs
@@ -10,19 +10,22 @@ input <- File.ReadAllText("input.txt");
 
 let allDifferentLetters (input: string) =
     let chars = input.ToCharArray() |> Seq.distinct |> Seq.toList
-    chars.Count() = 4
+    chars.Count() = input.Length
     
-let substr4 (input: string) (offset: int) = input.Substring(offset, 4)
+let substr (input: string) (seqLength: int) (offset: int) = input.Substring(offset, seqLength)
 
-let day6 (input: string) =
+let findSeq (input: string) (seqLength: int) =
     let markerSequence =
-        [0..input.Length-4]
-        |> Seq.map (substr4 input)
+        [0..input.Length-seqLength]
+        |> Seq.map (substr input seqLength)
         |> Seq.filter allDifferentLetters
         |> Seq.head
 
-    input.IndexOf markerSequence + 4        // be more efficient to capture the offset as we go rather than look it up again
+    input.IndexOf markerSequence + seqLength        // be more efficient to capture the offset as we go rather than look it up again
 
+
+let day6 (input: string) = findSeq input 4
+let day6part2 (input: string) = findSeq input 14
 
 
 printfn "%i" (day6 input)
@@ -30,3 +33,9 @@ printfn "%i" (day6 input)
 //printfn "%i" (day6 "nppdvjthqldpwncqszvftbrmjlhg")          // 6
 //printfn "%i" (day6 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")     // 10
 //printfn "%i" (day6 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")      // 11
+
+printfn "%i" (day6part2 input)
+//printfn "%i" (day6part2 "bvwbjplbgvbhsrlpgdmjqwftvncz")          // 23
+//printfn "%i" (day6part2 "nppdvjthqldpwncqszvftbrmjlhg")          // 23
+//printfn "%i" (day6part2 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")     // 29
+//printfn "%i" (day6part2 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")      // 26
