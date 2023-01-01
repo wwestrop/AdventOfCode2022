b7deba9d0ccd5dbf149396cff16029b8f1189fe2
Will Westrop <wwestrop@hotmail.com>
2023-01-01 14:03:17 +0000

    Day 13, part 2 (works for example input)

--- AdventOfCode2022/Day18.fs
+++ AdventOfCode2022/Day18.fs
@@ -1,6 +1,7 @@
 ﻿module mod_day18
 open System.IO
 open System
+open Helpers
 
 let mutable input = "2,2,2
 1,2,2
@@ -58,19 +59,20 @@ let isInBounds (x: int, y: int, z: int) (arr: 'T[,,]) =
         x < w && y < h && z < d
 
 
+let right = (1,0,0)
+let left = (-1,0,0)
+let up = (0,1,0)
+let down = (0,-1,0)
+let forward = (0,0,1)
+let backward = (0,0,-1)
+
+let allDirections = [right; left; up; down; forward; backward]
+
+
 let getAdjacentPositions (point: int * int * int) (arr: 'T[,,]) =
-
-    let offsetVectors = [
-        -1, 0, 0;
-         1, 0, 0;
-         0,-1, 0;
-         0, 1, 0;
-         0, 0,-1;
-         0, 0, 1;
-    ]
-
-    offsetVectors
+    allDirections
         |> Seq.map (fun offset -> addVec point offset)
+
 
 
 let day18 (input: string) =
@@ -82,4 +84,58 @@ let day18 (input: string) =
         |> Seq.length
 
 
+
+// Even checking for surrounding blocks in each direction won't work in all cases
+// Consider a 2D equivalent ( . = air, # = solid ):
+// 
+//   01234567
+// 0 ........
+// 1 ..#####.
+// 2 ..##.##.
+// 3 ..##.##.
+// 4 ..#..##.
+// 5 ..#.###.
+// 6 ..# ###.
+// 7 ........
+//
+// Consider (4,2). It has voxels both above and below it, and to the left and right
+// And if it was 3D, lets assume in front and behind it too.
+// But, the "kink" in the tunnel at (3,4) means that (4,2) is, in fact, exposed to air
+//
+// Do I need to Dijkstra or something to find the distance to any of the edges of the scanned volume?
+//
+let populateSurroundedVoxels (voxels: seq<int * int * int>) (arr: bool[,,]) =
+    let mutable copyVoxels = voxels |> Seq.toList
+    let w,h,d = arr.GetLength(0), arr.GetLength(1), arr.GetLength(2)
+
+    for x in 0..w-1 do
+        for y in 0..h-1 do
+            for z in 0..d-1 do
+                let isVoxelSurrounded =
+                    allDirections
+                    |> Seq.map (fun dir -> getCellsInDirection3D (x,y,z) dir arr)
+                    |> Seq.forall (fun x -> Seq.contains true x)
+
+                if isVoxelSurrounded then
+                    if arr[x,y,z] = false then
+                        arr[x,y,z] <- true
+                        copyVoxels <- (x,y,z) :: copyVoxels
+
+    copyVoxels
+
+
+let day18part2 (input: string) =
+
+    let grid, voxels = parse input
+
+    let voxels = populateSurroundedVoxels voxels grid
+
+    voxels
+    |> Seq.collect (fun v -> getAdjacentPositions v grid)
+    |> Seq.filter(fun (x,y,z) -> not(isInBounds (x,y,z) grid) || grid[x,y,z] = false)
+    |> Seq.length
+
+
+
 printfn "%i" (day18 input)
+printfn "%i" (day18part2 input)
---
+++ AdventOfCode2022/Helpers.fs
@@ -0,0 +1,31 @@
+﻿module Helpers
+
+    let getCellsInDirection (starting: int * int) (xoffset, yoffset) (arr: 'T[,]) =
+        let (w, h) = arr.GetLength(0), arr.GetLength(1)
+
+        seq {
+            let mutable x, y = starting
+            x <- x + xoffset
+            y <- y + yoffset
+
+            while x >= 0 && x <= w-1 && y >= 0 && y <= h-1 do
+                yield arr[x, y]
+                x <- x + xoffset
+                y <- y + yoffset
+        }
+
+    let getCellsInDirection3D (starting: int * int * int) (xoffset, yoffset, zoffset) (arr: 'T[,,]) =
+        let w, h, d = arr.GetLength(0), arr.GetLength(1), arr.GetLength(2)
+
+        seq {
+            let mutable x, y, z = starting
+            x <- x + xoffset
+            y <- y + yoffset
+            z <- z + zoffset
+
+            while x >= 0 && x <= w-1 && y >= 0 && y <= h-1 && z >=0 && z <= d-1 do
+                yield arr[x, y, z]
+                x <- x + xoffset
+                y <- y + yoffset
+                z <- z + zoffset
+        }
